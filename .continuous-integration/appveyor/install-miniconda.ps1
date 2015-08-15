bee961422daa2450eb708f9c05f463062297acff
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-08-15 21:58:09 +0200

    AppVeyor: Try and install OpenGL as done by the VisPy project

--- .continuous-integration/appveyor/install-miniconda.ps1
+++ .continuous-integration/appveyor/install-miniconda.ps1
@@ -1,9 +1,38 @@
 ﻿# Sample script to install anaconda under windows
 # Authors: Stuart Mumford
-# Borrwed from: Olivier Grisel and Kyle Kastner
+# Borrowed from: Olivier Grisel and Kyle Kastner
 # License: BSD 3 clause
 
+# Some modifications to this file copied from the VisPy project
+# which is released under a BSD 3-clause license
+
 $MINICONDA_URL = "http://repo.continuum.io/miniconda/"
+$MESA_GL_URL = "https://github.com/vispy/demo-data/raw/master/mesa/"
+
+function DownloadMesaOpenGL ($architecture) {
+    $webclient = New-Object System.Net.WebClient
+    $basedir = $pwd.Path + "\"
+    $filepath = $basedir + "opengl32.dll"
+    # Download and retry up to 3 times in case of network transient errors.
+    $url = $MESA_GL_URL + "opengl32_mingw_" + $architecture + ".dll"
+    Write-Host "Downloading" $url
+    $retry_attempts = 2
+    for($i=0; $i -lt $retry_attempts; $i++){
+        try {
+            $webclient.DownloadFile($url, $filepath)
+            break
+        }
+        Catch [Exception]{
+            Start-Sleep 1
+        }
+    }
+    if (Test-Path $filepath) {
+        Write-Host "File saved at" $filepath
+    } else {
+        # Retry once to get the error message if any at the last try
+        $webclient.DownloadFile($url, $filepath)
+    }
+}
 
 function DownloadMiniconda ($version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
@@ -65,6 +94,7 @@ function InstallMiniconda ($python_version, $architecture, $python_home) {
 }
 
 function main () {
+    DownloadMesaOpenGL $env:PYTHON_ARCH
     InstallMiniconda $env:MINICONDA_VERSION $env:PLATFORM $env:PYTHON
 }
 
