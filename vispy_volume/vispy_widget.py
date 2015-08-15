6709f8ee8452ec805c6088f5c81120c6a447f7e3
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-08-15 11:48:17 +0200

    Import colormaps from colormaps module

--- vispy_volume/vispy_widget.py
+++ vispy_volume/vispy_widget.py
@@ -7,24 +7,9 @@ from glue.external.qt import QtGui, QtCore
 from vispy import scene
 from vispy.color import get_colormaps, BaseColormap
 
+from .colormaps import TransFire, TransGrays
+
 __all__ = ['QtVispyWidget']
-
-
-# create colormaps that work well for translucent and additive volume rendering
-class TransFire(BaseColormap):
-    glsl_map = """
-    vec4 translucent_fire(float t) {
-        return vec4(pow(t, 0.5), t, t*t, max(0, t*1.05 - 0.05));
-    }
-    """
-
-
-class TransGrays(BaseColormap):
-    glsl_map = """
-    vec4 translucent_grays(float t) {
-        return vec4(t, t, t, t*0.05);
-    }
-    """
 
 
 class QtVispyWidget(QtGui.QWidget):
