4178e565bcdcabfafde39e0d93e4786ce36070ca
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-02-23 19:11:06 +0000

    Updated isosurface tests

--- glue_vispy_viewers/isosurface/tests/test_iso_glueViewer.py
+++
@@ -1,9 +0,0 @@
-from glue.external.qt import get_qapp
-from ..iso_glue_viewer import GlueIsoVispyViewer
-from glue.config import qt_client
-
-def test_viewer():
-
-    # v = GlueVispyViewer()
-
-    qt_client.add(GlueIsoVispyViewer)
