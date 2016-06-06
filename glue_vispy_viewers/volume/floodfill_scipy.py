423d7a7c50209437b9a619d736617ac6b069bfef
PennyQ <qianxuran@nao.cas.cn>
2016-06-06 14:20:06 -0400

    add the floodfill selection

---
+++ glue_vispy_viewers/volume/floodfill_scipy.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy.ndimage.measurements import label
+
+
+def floodfill_scipy(data, start_coords, threshold):
+
+    # Determine value at the starting coordinates
+    value = data[start_coords]
+
+    # Determine all pixels that match
+    mask = (data > value / threshold) & (data < value * threshold)
+
+    # Determine all individual chunks
+    labels, num_features = label(mask)
+
+    mask = labels == labels[start_coords]
+
+    return mask
--- glue_vispy_viewers/volume/volume_toolbar.py
+++ glue_vispy_viewers/volume/volume_toolbar.py
@@ -4,6 +4,7 @@ selection (not available now).
 """
 from ..common.toolbar import VispyDataViewerToolbar
 
+import math
 import numpy as np
 from glue.core.roi import RectangularROI, CircularROI
 from glue.utils.geometry import points_inside_poly
@@ -11,12 +12,15 @@ from ..utils import as_matrix_transform
 from matplotlib import path
 from ..extern.vispy import scene
 
+from floodfill_scipy import floodfill_scipy
+
 
 class VolumeSelectionToolbar(VispyDataViewerToolbar):
 
     def __init__(self, vispy_widget=None, parent=None):
         super(VolumeSelectionToolbar, self).__init__(vispy_widget=vispy_widget,
                                                      parent=parent)
+        # for getting transposed vol_data shape and getting pos array
         self.trans_ones_data = None
         # add some markers
         self.markers = scene.visuals.Markers(parent=self._vispy_widget.view.scene)
@@ -26,6 +30,8 @@ class VolumeSelectionToolbar(VispyDataViewerToolbar):
         self.visual_tr = None
         self.visible_data = None
         self.visual = None
+        self.vol_data = None
+        self.max_value_pos = None
 
 
 # todo: add global variable of self.visual, vol_data, vol_shape
@@ -39,55 +45,26 @@ class VolumeSelectionToolbar(VispyDataViewerToolbar):
             # do the initiation here
             self.selection_origin = event.pos
             self.visible_data, self.visual = self.get_visible_data()
+
             data_array = self.visible_data[0]['PRIMARY']
+            self.trans_ones_data = np.transpose(np.ones(data_array.shape))
+
+            self.vol_data = data_array
             self.visual_tr = self._vispy_widget.limit_transforms[self.visual]
 
         if self.mode is 'point':
-            trans = self.visual_tr.translate
-            scale = self.visual_tr.scale
 
             # get start and end point of ray line
             pos = self.get_ray_line()
-
-            inter_pos = []
-            for z in range(0, data_array.shape[0]):
-                #   3D line defined with two points (x0, y0, z0) and (x1, y1, z1) as
-                #   (x - x1)/(x2 - x1) = (y - y1)/(y2 - y1) = (z - z1)/(z2 - z1) = t
-                z = z * scale[2] + trans[2]
-                t = (z - pos[0][2])/(pos[1][2] - pos[0][2])
-                x = t * (pos[1][0] - pos[0][0]) + pos[0][0]
-                y = t * (pos[1][1] - pos[0][1]) + pos[0][1]
-                inter_pos.append([x, y, z])
-            inter_pos = np.array(inter_pos)
-
-            # cut the line within the cube
-            m1 = inter_pos[:, 0] > trans[0]  # for x
-            m2 = inter_pos[:, 0] < (data_array.shape[2] * scale[0] + trans[0])
-            m3 = inter_pos[:, 1] > trans[1]  # for y
-            m4 = inter_pos[:, 1] < (data_array.shape[1]*scale[1] + trans[1])
-            inter_pos = inter_pos[m1 & m2 & m3 & m4]
-
-            # set colors for markers
-            colors = np.ones((inter_pos.shape[0], 4))
-            colors[:] = (0.5, 0.5, 0, 1)
-
-            # value of intersected points
-            inter_value = []
-            for each_point in inter_pos:
-                x = (each_point[0] - trans[0])/scale[0]
-                y = (each_point[1] - trans[1])/scale[1]
-                z = (each_point[2] - trans[2])/scale[2]
-                inter_value.append(data_array[(z, y, x)])
-            inter_value = np.array(inter_value)
-
-            assert inter_value.shape[0] == inter_pos.shape[0]
+            max_value_pos, max_value = self.get_inter_value(pos)
+            self.max_value_pos = max_value_pos
 
             # change the color of max value marker
-            if len(inter_value) != 0:
-                self.markers.set_data(pos=np.array([inter_pos[np.argmax(inter_value)]]),
+            if max_value:
+                self.markers.set_data(pos=np.array(max_value_pos),
                                       face_color='yellow')
-                status_text = 'pos '+str(inter_pos[np.argmax(inter_value)]) \
-                              + ' value '+str(inter_value.max())
+                status_text = 'pos '+str(max_value_pos[0]) \
+                              + ' value '+str(max_value)
                 self._vispy_data_viewer.show_status(status_text)
 
             self._vispy_widget.canvas.update()
@@ -142,6 +119,50 @@ class VolumeSelectionToolbar(VispyDataViewerToolbar):
 
             self.lasso_reset()
 
+    def on_mouse_move(self, event):
+
+        super(VolumeSelectionToolbar, self).on_mouse_move(event=event)
+
+        if event.button == 1 and event.is_dragging and self.mode is not None:
+            if self.mode is 'point':
+
+                # calculate the threshold and call draw visual
+                width = event.pos[0] - self.selection_origin[0]
+                height = event.pos[1] - self.selection_origin[1]
+                drag_distance = math.sqrt(width**2+height**2)
+                canvas_diag = math.sqrt(self._vispy_widget.canvas.size[0]**2
+                                        + self._vispy_widget.canvas.size[1]**2)
+
+                mask = self.draw_floodfill_visual(drag_distance / canvas_diag)
+
+                if mask is not None:
+                    new_mask = np.reshape(mask, self.trans_ones_data.shape)
+                    new_mask = np.ravel(np.transpose(new_mask))
+                    self.mark_selected(new_mask, self.visible_data)
+
+    def draw_floodfill_visual(self, threshold):
+        formate_data = np.asarray(self.vol_data, np.float64)
+
+        # Normalize the threshold so that it returns values in the range 1.01
+        # to 101 (since it can currently be between 0 and 1)
+
+        threshold = 1 + 10 ** (threshold * 4 - 2)
+
+        trans = self.visual_tr.translate
+        scale = self.visual_tr.scale
+
+        max_value_pos = self.max_value_pos[0]
+        x = (max_value_pos[0] - trans[0])/scale[0]
+        y = (max_value_pos[1] - trans[1])/scale[1]
+        z = (max_value_pos[2] - trans[2])/scale[2]
+
+        if self.max_value_pos:
+            selec_mask = floodfill_scipy(formate_data, (z, y, x),
+                                         threshold).astype(float) * 5
+            return selec_mask
+        else:
+            return None
+
     def get_map_data(self):
         """
         Get the mapped buffer from self.visual to canvas.
@@ -160,9 +181,54 @@ class VolumeSelectionToolbar(VispyDataViewerToolbar):
         self.trans_ones_data = np.transpose(np.ones(data_object.data.shape))
 
         pos_data = np.indices(layer.data.shape[::-1], dtype=float).reshape(3, -1).transpose()
+
         data = tr.map(pos_data)
         data /= data[:, 3:]   # normalize with homogeneous coordinates
         return data[:, :2]
+
+    def get_inter_value(self, pos):
+        trans = self.visual_tr.translate
+        scale = self.visual_tr.scale
+
+        inter_pos = []
+        for z in range(0, self.vol_data.shape[0]):
+            #   3D line defined with two points (x0, y0, z0) and (x1, y1, z1) as
+            #   (x - x1)/(x2 - x1) = (y - y1)/(y2 - y1) = (z - z1)/(z2 - z1) = t
+            z = z * scale[2] + trans[2]
+            t = (z - pos[0][2])/(pos[1][2] - pos[0][2])
+            x = t * (pos[1][0] - pos[0][0]) + pos[0][0]
+            y = t * (pos[1][1] - pos[0][1]) + pos[0][1]
+            inter_pos.append([x, y, z])
+        inter_pos = np.array(inter_pos)
+
+        # cut the line within the cube
+        m1 = inter_pos[:, 0] > trans[0]  # for x
+        m2 = inter_pos[:, 0] < (self.vol_data.shape[2] * scale[0] + trans[0])
+        m3 = inter_pos[:, 1] > trans[1]  # for y
+        m4 = inter_pos[:, 1] < (self.vol_data.shape[1]*scale[1] + trans[1])
+        inter_pos = inter_pos[m1 & m2 & m3 & m4]
+
+        # set colors for markers
+        colors = np.ones((inter_pos.shape[0], 4))
+        colors[:] = (0.5, 0.5, 0, 1)
+
+        # value of intersected points
+        inter_value = []
+        for each_point in inter_pos:
+            x = (each_point[0] - trans[0])/scale[0]
+            y = (each_point[1] - trans[1])/scale[1]
+            z = (each_point[2] - trans[2])/scale[2]
+            inter_value.append(self.vol_data[(z, y, x)])
+        inter_value = np.array(inter_value)
+
+        assert inter_value.shape[0] == inter_pos.shape[0]
+
+        # TODO: there is a risk that no intersected points found here
+        if len(inter_pos) == 0 or len(inter_value) == 0:
+            print('empty selection list', inter_pos, inter_value)
+            return None, None
+        else:
+            return [inter_pos[np.argmax(inter_value)]], inter_value.max()
 
     def get_ray_line(self):
         """
