4b2d472dea6882c974418c19d2ebfd97135887cc
PennyQ <qianxuran@nao.cas.cn>
2016-07-02 16:29:36 -0400

    replace isosurface visual method to display

--- glue_vispy_viewers/isosurface/layer_artist.py
+++ glue_vispy_viewers/isosurface/layer_artist.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, division, print_function
 
 import numpy as np
 from matplotlib.colors import ColorConverter
+from scipy.ndimage import gaussian_filter
+
 
 from ..extern.vispy import scene
 from ..extern.vispy.color import Color
@@ -11,6 +13,148 @@ from glue.core.data import Subset
 from glue.core.layer_artist import LayerArtistBase
 from glue.utils import nonpartial
 from glue.core.exceptions import IncompatibleAttribute
+
+
+from ..extern.vispy.color import BaseColormap
+from ..extern.vispy.visuals.volume import frag_dict, FRAG_SHADER
+
+# Custom shader to replace existing iso one
+
+ISO_SNIPPETS = dict(
+    before_loop="""
+        vec4 total_color = vec4(0.0);  // final color
+        vec4 src = vec4(0.0);
+        vec4 dst = vec4(0.0);
+        vec3 dstep = 1.5 / u_shape;  // step to sample derivative
+        gl_FragColor = vec4(0.0);
+        float val_prev = 0;
+        float outa = 0;
+        vec3 loc_prev = vec3(0.0);
+        vec3 loc_mid = vec3(0.0);
+        float t1 = 0.6;
+        float t2 = 0.4;
+
+    """,
+    in_loop="""
+        if (val > t1 && val_prev < t1) {
+
+            // Use bisection to find correct position of contour
+            for (int i=0; i<20; i++) {
+                loc_mid = 0.5 * (loc_prev + loc);
+                val = $sample(u_volumetex, loc_mid).g;
+                if (val > u_threshold) {
+                    loc = loc_mid;
+                } else {
+                    loc_prev = loc_mid;
+                }
+            }
+
+            dst = $cmap(val);
+            dst = calculateColor(dst, loc, dstep);
+
+            src = total_color;
+
+            outa = src.a + dst.a * (1 - src.a);
+            total_color = (src * src.a + dst * dst.a * (1 - src.a)) / outa;
+            total_color.a = outa;
+        }
+
+        if (val < t1 && val_prev > t1) {
+
+            // Use bisection to find correct position of contour
+            for (int i=0; i<20; i++) {
+                loc_mid = 0.5 * (loc_prev + loc);
+                val = $sample(u_volumetex, loc_mid).g;
+                if (val < u_threshold) {
+                    loc = loc_mid;
+                } else {
+                    loc_prev = loc_mid;
+                }
+            }
+
+            dst = $cmap(val);
+            dst = calculateColor(dst, loc, dstep);
+
+            src = total_color;
+
+            outa = src.a + dst.a * (1 - src.a);
+            total_color = (src * src.a + dst * dst.a * (1 - src.a)) / outa;
+            total_color.a = outa;
+        }
+
+        if (val > t2 && val_prev < t2) {
+
+            // Use bisection to find correct position of contour
+            for (int i=0; i<20; i++) {
+                loc_mid = 0.5 * (loc_prev + loc);
+                val = $sample(u_volumetex, loc_mid).g;
+                if (val > u_threshold) {
+                    loc = loc_mid;
+                } else {
+                    loc_prev = loc_mid;
+                }
+            }
+
+            dst = $cmap(val);
+            dst = calculateColor(dst, loc, dstep);
+            dst.a = 0.25;
+
+            src = total_color;
+
+            outa = src.a + dst.a * (1 - src.a);
+            total_color = (src * src.a + dst * dst.a * (1 - src.a)) / outa;
+            total_color.a = outa;
+
+        }
+
+        if (val < t2 && val_prev > t2) {
+
+            // Use bisection to find correct position of contour
+            for (int i=0; i<20; i++) {
+                loc_mid = 0.5 * (loc_prev + loc);
+                val = $sample(u_volumetex, loc_mid).g;
+                if (val < u_threshold) {
+                    loc = loc_mid;
+                } else {
+                    loc_prev = loc_mid;
+                }
+            }
+
+            dst = $cmap(val);
+            dst = calculateColor(dst, loc, dstep);
+            dst.a = 0.25;
+
+            src = total_color;
+
+            outa = src.a + dst.a * (1 - src.a);
+            total_color = (src * src.a + dst * dst.a * (1 - src.a)) / outa;
+            total_color.a = outa;
+
+        }
+
+        val_prev = val;
+        loc_prev = loc;
+        """,
+    after_loop="""
+        gl_FragColor = total_color;
+        """,
+)
+
+ISO_FRAG_SHADER = FRAG_SHADER.format(**ISO_SNIPPETS)
+
+frag_dict['iso'] = ISO_FRAG_SHADER
+
+
+class TwoLevel(BaseColormap):
+    glsl_map = """
+    vec4 translucent_fire(float t) {{
+        if(t > 0.5) {
+            return vec4(0.3, 0.0, 0.6, 1.0);
+        } else {
+            return vec4(0.43, 0.09, 0.43, 0.5);
+        }
+    }}
+    """
 
 
 class IsosurfaceLayerArtist(LayerArtistBase):
@@ -30,7 +174,10 @@ class IsosurfaceLayerArtist(LayerArtistBase):
         self.layer = layer
         self.vispy_viewer = vispy_viewer
 
-        self._iso_visual = scene.Isosurface(np.ones((3, 3, 3)), level=0.5, shading='smooth')
+        # self._iso_visual = scene.Isosurface(np.ones((3, 3, 3)), level=0.5, shading='smooth')
+        # Create isosurface visual
+        self._iso_visual = scene.visuals.Volume(np.ones((3, 3, 3)), method='iso', relative_step_size=0.5)
+        # relative_step_size: ray casting performance, recommond 0.5~1.5)
         self.vispy_viewer.add_data_visual(self._iso_visual)
         self._vispy_color = None
 
@@ -78,20 +225,27 @@ class IsosurfaceLayerArtist(LayerArtistBase):
         self._changed = False
 
     def _update_level(self):
-        self._iso_visual.level = self.level
+        self._iso_visual.threshold = self.level
         self.redraw()
 
     def _update_color(self):
         self._update_vispy_color()
         if self._vispy_color is not None:
             self._iso_visual.color = self._vispy_color
+        # TODO: accept color from ui
+        self._iso_visual.cmap = TwoLevel()
+        # self._update_vispy_color()
+        # self._iso_visual.color = self._vispy_color
         self.redraw()
+        pass
 
     def _update_vispy_color(self):
-        if self.color is None:
-            return
-        self._vispy_color = Color(ColorConverter().to_rgb(self.color))
-        self._vispy_color.alpha = self.alpha
+        # TODO: add cmap that contains color numbers as same as isosurface shell number
+        # if self.color is None:
+        #     return
+        # self._vispy_color = Color(ColorConverter().to_rgb(self.color))
+        # self._vispy_color.alpha = self.alpha
+        pass
 
     def _update_data(self):
         if isinstance(self.layer, Subset):
@@ -117,7 +271,11 @@ class IsosurfaceLayerArtist(LayerArtistBase):
             data[:kmin] = invalid
             data[kmax:] = invalid
 
-        self._iso_visual.set_data(np.nan_to_num(data).transpose())
+        # self._iso_visual.set_data(np.nan_to_num(data).transpose())
+        # TODO: gaussian smooth for the data, why multiply by 4?
+        gaussian_data = gaussian_filter(data/4, 1)
+
+        self._iso_visual.set_data(np.nan_to_num(gaussian_data))  # or .transpose()?
         self.redraw()
 
     def _update_visibility(self):
--- glue_vispy_viewers/isosurface/layer_style_widget.py
+++ glue_vispy_viewers/isosurface/layer_style_widget.py
@@ -23,6 +23,8 @@ class IsosurfaceLayerStyleWidget(QtWidgets.QWidget):
     # GUI elements
     attribute = CurrentComboProperty('ui.combo_attribute')
     level = FloatLineProperty('ui.value_level')
+    # We don't have IntLineProperty?
+    step_num = FloatLineProperty('ui.step_num')
     alpha = ValueProperty('ui.slider_alpha')
 
     def __init__(self, layer_artist):
