464a323ce7b9d7d3f2578d87153b56749ba82d9a
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-08-11 16:31:58 +0100

    On Windows, import vispy.gloo before doing anything else, otherwise things can fail if PyQt4.Qt is imported before vispy.gloo is set up.

--- glue_vispy_viewers/__init__.py
+++ glue_vispy_viewers/__init__.py
@@ -1,3 +1,9 @@
+# Import vispy.gloo first when on Windows otherwise there are strange
+# side-effects when PyQt4.Qt is imported first (which it now is in QtPy)
+import sys
+if sys.platform.startswith('win'):
+    import glue_vispy_viewers.extern.vispy.gloo.gl
+
 from .version import __version__
 
 BACKGROUND_COLOR = 'black'
