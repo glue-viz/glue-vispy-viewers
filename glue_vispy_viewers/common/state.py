c6f2e236792e915a3c446682716445a7f1831e43
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-02-06 14:46:00 +0000

    Add callback if limits_cache is changed

--- glue_vispy_viewers/common/state.py
+++ glue_vispy_viewers/common/state.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from glue.external.echo import CallbackProperty, delay_callback, ListCallbackProperty
 from glue.core.state_objects import State, StateAttributeLimitsHelper
+from glue.utils import nonpartial
 
 __all__ = ['Vispy3DViewerState']
 
@@ -61,6 +62,21 @@ class Vispy3DViewerState(State):
         self.z_att_helper = StateAttributeLimitsHelper(self, attribute='z_att',
                                                        lower='z_min', upper='z_max',
                                                        cache=self.limits_cache)
+
+        # TODO: if limits_cache is re-assigned to a different object, we need to
+        # update the attribute helpers. However if in future we make limits_cache
+        # into a smart dictionary that can call callbacks when elements are
+        # changed then we shouldn't always call this. It'd also be nice to
+        # avoid this altogether and make it more clean.
+        self.add_callback('limits_cache', nonpartial(self._update_limits_cache))
+
+    def _update_limits_cache(self):
+        self.x_att_helper._cache = self.limits_cache
+        self.x_att_helper._update_attribute()
+        self.y_att_helper._cache = self.limits_cache
+        self.y_att_helper._update_attribute()
+        self.z_att_helper._cache = self.limits_cache
+        self.z_att_helper._update_attribute()
 
     @property
     def aspect(self):
