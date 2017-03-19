e0c4c95dcff38ad7361a10a6f53952a0306d34ca
PennyQ <qianxuran@nao.cas.cn>
2017-03-19 11:52:33 -0400

    fix flake8

--- glue_vispy_viewers/common/vispy_widget.py
+++ glue_vispy_viewers/common/vispy_widget.py
@@ -141,4 +141,3 @@ class VispyWidgetHelper(object):
         self.axis.xlim = self.viewer_state.x_min, self.viewer_state.x_max
         self.axis.ylim = self.viewer_state.y_min, self.viewer_state.y_max
         self.axis.zlim = self.viewer_state.z_min, self.viewer_state.z_max
-
