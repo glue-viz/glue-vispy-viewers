407fab21f63a93b191143a774cd7abbeed496c93
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-08-02 11:56:24 +0100

    Fix typo that caused color to not sync properly in 3D scatter viewer

--- glue_vispy_viewers/scatter/layer_state.py
+++ glue_vispy_viewers/scatter/layer_state.py
@@ -70,7 +70,7 @@ class ScatterLayerState(VispyLayerState):
 
         if self.layer is not None:
             self.size = self.layer.style.markersize
-            self._sync_color = keep_in_sync(self, 'size', self.layer.style, 'markersize')
+            self._sync_markersize = keep_in_sync(self, 'size', self.layer.style, 'markersize')
 
     def flip_size(self):
         self.size_att_helper.flip_limits()
