dda167e8ea0551e2d5cad850eed26e7e5c4f0c5f
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-01-09 14:40:33 +0000

    Update error message with proper link

--- glue_vispy_viewers/common/vispy_data_viewer.py
+++ glue_vispy_viewers/common/vispy_data_viewer.py
@@ -20,8 +20,8 @@ from .viewer_state import Vispy3DViewerState
 from .compat import update_viewer_state
 
 BROKEN_PYQT5_MESSAGE = ("The version of PyQt5 you are using does not appear to "
-                        "support OpenGL. See <a href='http://www.google.com'>this page</a> for "
-                        "more information about fixing this.")
+                        "support OpenGL. See <a href='http://docs.glueviz.org/en/stable/known_issues.html#d-viewers-not-working-on-linux-with-pyqt5'>"
+                        "here</a> for more information about fixing this issue.")
 
 
 class BaseVispyViewer(DataViewer):
