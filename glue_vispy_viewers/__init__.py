30c6dea7b3ed35162e913540729fef8d7af6dcab
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-08-13 14:23:39 +0100

    Test

--- glue_vispy_viewers/__init__.py
+++ glue_vispy_viewers/__init__.py
@@ -3,9 +3,15 @@ from .version import __version__  # noqa
 BACKGROUND_COLOR = 'black'
 FOREGROUND_COLOR = 'white'
 
-try:
-    import OpenGL  # noqa
-except ImportError:
-    raise ImportError("The PyOpenGL package is required for this plugin")
-else:
-    del OpenGL
+
+import ctypes
+ctypes.CDLL("libGL.so.1", mode=ctypes.RTLD_GLOBAL)
+
+print("HERE")
+
+#try:
+#    import OpenGL  # noqa
+#except ImportError:
+# #   raise ImportError("The PyOpenGL package is required for this plugin")
+#else:
+#    del OpenGL
