63e1d9ca5bcdee762f93b6ae57ae516bd86e4175
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-06-22 15:16:57 +0100

    Switch to using setup.cfg and setuptools_scm, and add basic tox.ini configuration

--- glue_vispy_viewers/version.py
+++ glue_vispy_viewers/version.py
@@ -1,1 +1,8 @@
-__version__ = '0.12.dev0'
+from __future__ import absolute_import, division, print_function
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = 'undefined'
