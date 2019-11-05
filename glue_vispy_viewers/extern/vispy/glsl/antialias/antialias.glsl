0d9a16d708d91712965a2bdfd2abee2e17a97b7b
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-11-05 15:42:44 +0000

    Remove bundled vispy

--- glue_vispy_viewers/extern/vispy/glsl/antialias/antialias.glsl
+++
@@ -1,7 +0,0 @@
-// ----------------------------------------------------------------------------
-// Copyright (c) 2014, Nicolas P. Rougier. All Rights Reserved.
-// Distributed under the (new) BSD License.
-// ----------------------------------------------------------------------------
-#include "antialias/stroke.glsl"
-#include "antialias/filled.glsl"
-#include "antialias/outline.glsl"
--- glue_vispy_viewers/extern/vispy/visuals/glsl/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Repository of common GLSL functions."""
