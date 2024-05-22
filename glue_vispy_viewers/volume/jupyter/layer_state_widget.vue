c61e45424c0c0bcb07e118cd3394d0d6a4f2a539
Thomas Robitaille <thomas.robitaille@gmail.com>
2024-05-22 15:12:07 +0100

    Added Jupyter widgets for volume viewer

---
+++ glue_vispy_viewers/volume/jupyter/layer_state_widget.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div>
+            <v-select label="attribute" :items="attribute_items" v-model="attribute_selected" hide-details class="margin-bottom: 16px" />
+        </div>
+        <div>
+            <v-subheader class="pl-0 slider-label">opacity</v-subheader>
+            <glue-throttled-slider wait="300" max="1" step="0.01" :value.sync="glue_state.alpha" hide-details />
+        </div>
+        <div>
+            <glue-float-field label="min" :value.sync="glue_state.vmin" />
+        </div>
+        <div>
+            <glue-float-field label="max" :value.sync="glue_state.vmax" />
+        </div>
+    </div>
+</template>
+<script>
+    modules.export = {
+        methods: {
+            setAlpha(value) {
+                this.glue_state.alpha = value;
+            },
+        },
+    }
+</script>
+<style id="layer_volume">
+    .v-subheader.slider-label {
+        font-size: 12px;
+        height: 16px;
+    }
+</style>
