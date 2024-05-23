946a20a21d7b6d9a296443a1c96751fa662dd271
Thomas Robitaille <thomas.robitaille@gmail.com>
2024-05-23 14:58:33 +0100

    FIx save/record tools

--- glue_vispy_viewers/common/jupyter/toolbar.py
+++ glue_vispy_viewers/common/jupyter/toolbar.py
@@ -14,3 +14,9 @@ class VispyJupyterToolbar(VispyViewerToolbarMixin, BasicJupyterToolbar):
     @property
     def viewer(self):
         return self._viewer()
+
+    def activate_tool(self, mode):
+        self._enable_tool_interactions(mode)
+
+    def deactivate_tool(self, mode):
+        self._disable_tool_interactions(mode)
--- glue_vispy_viewers/common/qt/toolbar.py
+++ glue_vispy_viewers/common/qt/toolbar.py
@@ -15,3 +15,11 @@ class VispyQtToolbar(VispyViewerToolbarMixin, BasicToolbar):
     @property
     def viewer(self):
         return self._viewer()
+
+    def activate_tool(self, mode):
+        self._enable_tool_interactions(mode)
+        super().activate_tool(mode)
+
+    def deactivate_tool(self, mode):
+        self._disable_tool_interactions(mode)
+        super().deactivate_tool(mode)
