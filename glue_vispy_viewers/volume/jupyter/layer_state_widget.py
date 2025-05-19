2510d07458a05031e017f22906c67d2058fc76e6
Carifio24 <carifio24@gmail.com>
2025-05-19 15:58:55 -0400

    Add colormap UI widgets to Jupyter volume viewer.

--- glue_vispy_viewers/volume/jupyter/layer_state_widget.py
+++ glue_vispy_viewers/volume/jupyter/layer_state_widget.py
@@ -1,6 +1,8 @@
 import ipyvuetify as v
 import traitlets
 
+from glue.config import colormaps
+
 from glue_jupyter.state_traitlets_helpers import GlueState
 from glue_jupyter.vuetify_helpers import link_glue_choices
 
@@ -16,6 +18,13 @@ class Volume3DLayerStateWidget(v.VuetifyTemplate):
     attribute_items = traitlets.List().tag(sync=True)
     attribute_selected = traitlets.Int(allow_none=True).tag(sync=True)
 
+    # Color
+
+    color_mode_items = traitlets.List().tag(sync=True)
+    color_mode_selected = traitlets.Int(allow_none=True).tag(sync=True)
+
+    cmap_items = traitlets.List().tag(sync=True)
+
     def __init__(self, layer_state):
         super().__init__()
 
@@ -23,3 +32,16 @@ class Volume3DLayerStateWidget(v.VuetifyTemplate):
         self.glue_state = layer_state
 
         link_glue_choices(self, layer_state, "attribute")
+        link_glue_choices(self, layer_state, "color_mode")
+
+        self.cmap_items = [
+            {"text": cmap[0], "value": cmap[1].name} for cmap in colormaps.members
+        ]
+
+    def vue_set_colormap(self, data):
+        cmap = None
+        for member in colormaps.members:
+            if member[1].name == data:
+                cmap = member[1]
+                break
+        self.layer_state.cmap = cmap
