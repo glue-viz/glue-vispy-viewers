485c9a658bf9e2b6bd2caac080afc1a0f351e75b
Thomas Robitaille <thomas.robitaille@gmail.com>
2026-02-06 15:06:05 +0000

    Fix codestyle

--- glue_vispy_viewers/scatter/viewer_state.py
+++ glue_vispy_viewers/scatter/viewer_state.py
@@ -5,8 +5,9 @@ from glue.viewers3d.scatter.viewer_state import ScatterViewerState3D
 __all__ = ['Vispy3DScatterViewerState']
 
 warnings.warn(
-    "Importing Vispy3DScatterViewerState from glue_vispy_viewers.scatter.viewer_state is deprecated. "
-    "Please import ScatterViewerState3D from glue.viewers3d.scatter.viewer_state instead.",
+    "Importing Vispy3DScatterViewerState from glue_vispy_viewers.scatter.viewer_state is "
+    "deprecated. Please import ScatterViewerState3D from glue.viewers3d.scatter.viewer_state "
+    "instead.",
     DeprecationWarning,
     stacklevel=2
 )
--- glue_vispy_viewers/volume/layer_artist.py
+++ glue_vispy_viewers/volume/layer_artist.py
@@ -1,7 +1,5 @@
 import uuid
 
-import numpy as np
-
 from matplotlib.colors import ColorConverter
 
 from glue.core.data import Subset, Data
