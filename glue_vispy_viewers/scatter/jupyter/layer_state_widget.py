2fd0e34b634dcde9b6a931cee50f3b90533cf278
Carifio24 <carifio24@gmail.com>
2026-05-17 13:29:35 -0400

    Update scatter widgets to use echo Vue autoconnections.

--- glue_vispy_viewers/scatter/jupyter/layer_state_widget.py
+++ glue_vispy_viewers/scatter/jupyter/layer_state_widget.py
@@ -1,10 +1,8 @@
 import ipyvuetify as v
-import traitlets
 
-from glue.config import colormaps
+from echo.vue import autoconnect_callbacks_to_vue
+from glue_jupyter.vuetify_helpers import cmap_extras
 
-from glue_jupyter.state_traitlets_helpers import GlueState
-from glue_jupyter.vuetify_helpers import link_glue_choices
 
 __all__ = ["Scatter3DLayerStateWidget"]
 
@@ -13,89 +11,10 @@ class Scatter3DLayerStateWidget(v.VuetifyTemplate):
 
     template_file = (__file__, "layer_state_widget.vue")
 
-    glue_state = GlueState().tag(sync=True)
-
-    # Color
-
-    color_mode_items = traitlets.List().tag(sync=True)
-    color_mode_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    cmap_att_items = traitlets.List().tag(sync=True)
-    cmap_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    cmap_items = traitlets.List().tag(sync=True)
-
-    # Points
-
-    size_mode_items = traitlets.List().tag(sync=True)
-    size_mode_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    size_att_items = traitlets.List().tag(sync=True)
-    size_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    # Line
-
-    linestyle_items = traitlets.List().tag(sync=True)
-    linestyle_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    # Vectors
-
-    vx_att_items = traitlets.List().tag(sync=True)
-    vx_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    vy_att_items = traitlets.List().tag(sync=True)
-    vy_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    vz_att_items = traitlets.List().tag(sync=True)
-    vz_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    # Errors
-
-    xerr_att_items = traitlets.List().tag(sync=True)
-    xerr_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    yerr_att_items = traitlets.List().tag(sync=True)
-    yerr_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
-    zerr_att_items = traitlets.List().tag(sync=True)
-    zerr_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
-
     def __init__(self, layer_state):
         super().__init__()
 
-        self.layer_state = layer_state
-        self.glue_state = layer_state
+        self.state = layer_state
 
-        # Color
-
-        link_glue_choices(self, layer_state, "color_mode")
-        link_glue_choices(self, layer_state, "cmap_att")
-
-        self.cmap_items = [
-            {"text": cmap[0], "value": cmap[1].name} for cmap in colormaps.members
-        ]
-
-        # Size
-
-        link_glue_choices(self, layer_state, "size_mode")
-        link_glue_choices(self, layer_state, "size_att")
-
-        # Vectors
-
-        link_glue_choices(self, layer_state, "vx_att")
-        link_glue_choices(self, layer_state, "vy_att")
-        link_glue_choices(self, layer_state, "vz_att")
-
-        # Error bars
-
-        link_glue_choices(self, layer_state, "xerr_att")
-        link_glue_choices(self, layer_state, "yerr_att")
-        link_glue_choices(self, layer_state, "zerr_att")
-
-    def vue_set_colormap(self, data):
-        cmap = None
-        for member in colormaps.members:
-            if member[1].name == data:
-                cmap = member[1]
-                break
-        self.layer_state.cmap = cmap
+        extras = {"cmap": cmap_extras(self)}
+        autoconnect_callbacks_to_vue(layer_state, self, extras=extras)
--- glue_vispy_viewers/scatter/jupyter/layer_state_widget.vue
+++ glue_vispy_viewers/scatter/jupyter/layer_state_widget.vue
@@ -4,48 +4,48 @@
         <div>
             <v-select label="color" :items="color_mode_items" v-model="color_mode_selected" hide-details />
         </div>
-        <template v-if="glue_state.color_mode === 'Linear'">
+        <template v-if="(color_mode_items[color_mode_selected] || {}).text === 'Linear'">
             <div>
                 <v-select label="attribute" :items="cmap_att_items" v-model="cmap_att_selected" hide-details />
             </div>
             <div>
-                <glue-float-field label="min" :value.sync="glue_state.cmap_vmin" />
+                <glue-float-field label="min" :value.sync="cmap_vmin" echo-type="float" />
             </div>
             <div>
-                <glue-float-field label="max" :value.sync="glue_state.cmap_vmax" />
+                <glue-float-field label="max" :value.sync="cmap_vmax" echo-type="float" />
             </div>
             <div>
-                <v-select label="colormap" :items="cmap_items" :value="glue_state.cmap" @change="set_colormap" hide-details/>
+                <v-select label="colormap" :items="cmap_items" v-model="cmap" hide-details />
             </div>
         </template>
         <div>
             <v-subheader class="pl-0 slider-label">opacity</v-subheader>
-            <glue-throttled-slider wait="300" min="0" max="1" step="0.01" :value.sync="glue_state.alpha" hide-details />
+            <glue-throttled-slider wait="300" min="0" max="1" step="0.01" :value.sync="alpha" echo-type="float" hide-details />
         </div>
         <div class="text-subtitle-2 font-weight-bold">Points</div>
         <v-select label="size" :items="size_mode_items" v-model="size_mode_selected" hide-details />
-        <template v-if="glue_state.size_mode === 'Linear'">
+        <template v-if="(size_mode_items[size_mode_selected] || {}).text === 'Linear'">
             <div>
                 <v-select label="attribute" :items="size_att_items" v-model="size_att_selected" hide-details />
             </div>
             <div>
-                <glue-float-field label="min" :value.sync="glue_state.size_vmin" />
+                <glue-float-field label="min" :value.sync="size_vmin" echo-type="float" />
             </div>
             <div>
-                <glue-float-field label="max" :value.sync="glue_state.size_vmax" />
+                <glue-float-field label="max" :value.sync="size_vmax" echo-type="float" />
             </div>
         </template>
         <div>
             <v-subheader class="pl-0 slider-label">size scaling</v-subheader>
-            <glue-throttled-slider wait="300" min="0.1" max="10" step="0.01" :value.sync="glue_state.size_scaling"
+            <glue-throttled-slider wait="300" min="0.1" max="10" step="0.01" :value.sync="size_scaling" echo-type="float"
                 hide-details />
         </div>
-        <div class="text-subtitle-2 font-weight-bold" :style="glue_state.markers_visible ? {} : {marginTop: '6px'}">Vectors</div>
+        <div class="text-subtitle-2 font-weight-bold" style="margin-top: 6px;">Vectors</div>
         <div>
             <v-subheader class="pl-0 slider-label">show vectors</v-subheader>
-            <v-switch v-model="glue_state.vector_visible" hide-details style="margin-top: 0"/>
+            <v-switch v-model="vector_visible" hide-details style="margin-top: 0"/>
         </div>
-        <template v-if="glue_state.vector_visible">
+        <template v-if="vector_visible">
             <div>
                 <v-select label="vx" :items="vx_att_items" v-model="vx_att_selected" hide-details />
             </div>
@@ -56,38 +56,37 @@
                 <v-select label="vy" :items="vz_att_items" v-model="vz_att_selected" hide-details />
             </div>
         </template>
-        <div class="text-subtitle-2 font-weight-bold" :style="glue_state.vector_visible ? {} : {marginTop: '6px'}">Errorbars</div>
+        <div class="text-subtitle-2 font-weight-bold" :style="vector_visible ? {} : {marginTop: '6px'}">Errorbars</div>
         <div>
             <v-subheader class="pl-0 slider-label">show x errors</v-subheader>
-            <v-switch v-model="glue_state.xerr_visible" hide-details style="margin-top: 0"/>
+            <v-switch v-model="xerr_visible" hide-details style="margin-top: 0"/>
         </div>
-        <template v-if="glue_state.xerr_visible">
+        <template v-if="xerr_visible">
             <div>
                 <v-select label="xerr" :items="xerr_att_items" v-model="xerr_att_selected" hide-details />
             </div>
         </template>
         <div>
             <v-subheader class="pl-0 slider-label">show y errors</v-subheader>
-            <v-switch v-model="glue_state.yerr_visible" hide-details style="margin-top: 0"/>
+            <v-switch v-model="yerr_visible" hide-details style="margin-top: 0"/>
         </div>
-        <template v-if="glue_state.yerr_visible">
+        <template v-if="yerr_visible">
             <div>
                 <v-select label="yerr" :items="yerr_att_items" v-model="yerr_att_selected" hide-details />
             </div>
         </template>
         <div>
             <v-subheader class="pl-0 slider-label">show z errors</v-subheader>
-            <v-switch v-model="glue_state.zerr_visible" hide-details style="margin-top: 0"/>
+            <v-switch v-model="zerr_visible" hide-details style="margin-top: 0"/>
         </div>
-        <template v-if="glue_state.zerr_visible">
+        <template v-if="zerr_visible">
             <div>
                 <v-select label="zerr" :items="zerr_att_items" v-model="zerr_att_selected" hide-details />
             </div>
         </template>
     </div>
 </template>
-<script>
-</script>
+
 <style id="layer_scatter">
 .glue-layer-scatter .v-subheader.slider-label {
     font-size: 12px;
